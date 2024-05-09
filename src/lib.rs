5460714d841f66c00026f131a3156c77c85ec541
xutianyi <xutianyi1999@live.com>
2024-05-09 15:07:35 +0800

    init

---
+++ src/lib.rs
@@ -0,0 +1,337 @@
+#[macro_use]
+extern crate log;
+
+use std::ffi::{c_char, c_void};
+use std::fs;
+use std::io::{self, BufRead, Cursor, Read};
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr};
+use std::ops::Deref;
+use std::str::FromStr;
+use std::sync::atomic::{AtomicI64, Ordering};
+use std::sync::Arc;
+use std::time::Duration;
+
+use anyhow::{Context, Result};
+use arc_swap::{ArcSwap, Cache};
+use chrono::Utc;
+use futures_util::stream::{SplitSink, SplitStream};
+use futures_util::{SinkExt, StreamExt};
+use ipnet::Ipv4Net;
+use iprange::IpRange;
+use net::get_interface_addr;
+use netstack_lwip::NetStack;
+use tokio::net::{TcpSocket, UdpSocket};
+use tokio::runtime::Runtime;
+
+use crate::net::{find_interface, get_ip_dst_addr, SocketExt};
+
+mod net;
+
+#[repr(C)]
+#[derive(Copy, Clone, Eq, PartialEq)]
+enum Direction {
+    Output,
+    Input,
+}
+
+type InterfacesInfo = extern "C" fn(ctx: *const c_void, info_json: *mut c_char);
+type PacketSend =
+    extern "C" fn(ctx: *const c_void, direction: Direction, packet: *const u8, len: usize);
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+struct ExternalContext {
+    ctx: *const c_void,
+    interfaces_info_fn: InterfacesInfo,
+    packet_send_fn: PacketSend,
+}
+
+unsafe impl Send for ExternalContext {}
+unsafe impl Sync for ExternalContext {}
+
+enum Rule {
+    Match(IpRange<Ipv4Net>),
+    #[allow(unused)]
+    NotMatch(IpRange<Ipv4Net>),
+}
+
+impl Rule {
+    fn is_proxy(&self, target: &Ipv4Addr) -> bool {
+        match &self {
+            Rule::Match(range) => range.contains(target),
+            Rule::NotMatch(range) => !range.contains(target),
+        }
+    }
+}
+
+async fn udp_inbound_handler(
+    udp_inbound: Box<netstack_lwip::UdpSocket>,
+    device: Arc<String>,
+) -> Result<()> {
+    let mapping: Arc<ArcSwap<Vec<Arc<(SocketAddr, UdpSocket, AtomicI64)>>>> =
+        Arc::new(ArcSwap::from_pointee(Vec::new()));
+    let (tx, mut rx) = netstack_lwip::UdpSocket::split(udp_inbound);
+    let tx = Arc::new(tx);
+
+    let mut mapping_cache = Cache::new(&*mapping);
+
+    while let Some((pkt, from, to)) = rx.next().await {
+        let snap = mapping_cache.load();
+
+        let item = snap
+            .binary_search_by_key(&from, |v| (**v).0)
+            .ok()
+            .map(|i| &*snap.deref()[i]);
+
+        let insert_item;
+
+        let (_, to_socket, update_time) = match item {
+            None => {
+                let bind_addr = match to {
+                    SocketAddr::V4(_) => SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), 0),
+                    SocketAddr::V6(_) => SocketAddr::new(IpAddr::V6(Ipv6Addr::UNSPECIFIED), 0),
+                };
+
+                let to_socket = UdpSocket::bind(bind_addr).await?;
+                SocketExt::bind_device(&to_socket, &device, to.is_ipv6())?;
+                insert_item = Arc::new((from, to_socket, AtomicI64::new(Utc::now().timestamp())));
+
+                mapping.rcu(|v| {
+                    let mut tmp = (**v).clone();
+
+                    match tmp.binary_search_by_key(&from, |v| (**v).0) {
+                        Ok(_) => unreachable!(),
+                        Err(i) => tmp.insert(i, insert_item.clone()),
+                    }
+                    tmp
+                });
+
+                tokio::spawn({
+                    let tx = tx.clone();
+                    let mapping = mapping.clone();
+                    let insert_item = insert_item.clone();
+
+                    async move {
+                        let (_, to_socket, update_time) = &*insert_item;
+                        let mut buff = vec![0u8; 65536];
+
+                        let fut1 = async {
+                            loop {
+                                let (len, peer) = to_socket.recv_from(&mut buff).await?;
+                                debug!("recv from {} to {}", to, from);
+                                tx.send_to(&buff[..len], &peer, &from)?;
+                                update_time.store(Utc::now().timestamp(), Ordering::Relaxed);
+                            }
+                        };
+
+                        let fut2 = async {
+                            loop {
+                                tokio::time::sleep(Duration::from_secs(5)).await;
+
+                                if Utc::now().timestamp() - update_time.load(Ordering::Relaxed)
+                                    > 300
+                                {
+                                    return;
+                                }
+                            }
+                        };
+
+                        let res: io::Result<()> = tokio::select! {
+                            res = fut1 => res,
+                            _ = fut2 => Ok(())
+                        };
+
+                        if let Err(e) = res {
+                            error!("child udp handler error: {}", e);
+                        }
+
+                        mapping.rcu(|v| {
+                            let mut tmp = (**v).clone();
+
+                            match tmp.binary_search_by_key(&from, |v| (**v).0) {
+                                Ok(i) => tmp.remove(i),
+                                Err(_) => unreachable!(),
+                            };
+                            tmp
+                        });
+                    }
+                });
+
+                &*insert_item
+            }
+            Some(v) => v,
+        };
+
+        debug!("{} send to {}", from, to);
+        to_socket.send(&pkt).await?;
+        update_time.store(Utc::now().timestamp(), Ordering::Relaxed);
+    }
+    Ok(())
+}
+
+async fn tcp_inbound_handler(
+    mut listener: netstack_lwip::TcpListener,
+    device: Arc<String>,
+) -> Result<()> {
+    while let Some((mut inbound_stream, _local_addr, remote_addr)) = listener.next().await {
+        let device = device.clone();
+
+        tokio::spawn(async move {
+            let fut = async {
+                let socket = if remote_addr.is_ipv4() {
+                    TcpSocket::new_v4()?
+                } else {
+                    TcpSocket::new_v6()?
+                };
+
+                SocketExt::bind_device(&socket, &device, remote_addr.is_ipv6())?;
+
+                let mut outbound_stream = socket
+                    .connect(remote_addr)
+                    .await
+                    .with_context(|| format!("connect to {} error", &remote_addr))?;
+
+                tokio::io::copy_bidirectional(&mut inbound_stream, &mut outbound_stream).await?;
+                Result::<_, anyhow::Error>::Ok(())
+            };
+
+            if let Err(e) = fut.await {
+                error!("tcp_inbound_handler error: {:?}", e);
+            }
+        });
+    }
+    Ok(())
+}
+
+async fn netstatck_handler(mut stack_stream: SplitStream<NetStack>, ectx: ExternalContext) {
+    while let Some(pkt) = stack_stream.next().await {
+        if let Ok(pkt) = pkt {
+            (ectx.packet_send_fn)(ectx.ctx, Direction::Input, pkt.as_ptr(), pkt.len());
+        }
+    }
+}
+
+async fn netstatck_sink_handler(
+    mut netstack_sink: SplitSink<NetStack, Vec<u8>>,
+    mut rx: tokio::sync::mpsc::Receiver<Vec<u8>>,
+) -> Result<()> {
+    while let Some(pkg) = rx.recv().await {
+        netstack_sink.send(pkg).await?;
+    }
+    Ok(())
+}
+
+struct HookHandle {
+    _rt: Runtime,
+    netstatck_sink_handler_tx: tokio::sync::mpsc::Sender<Vec<u8>>,
+    rule: Rule,
+}
+
+fn parse_rules(file_path: &str) -> Result<IpRange<Ipv4Net>> {
+    let mut file = fs::File::open(file_path)?;
+    let mut buff = Vec::with_capacity(file.metadata()?.len() as usize);
+    file.read_to_end(&mut buff)?;
+
+    let mut lines = Cursor::new(buff).lines();
+    let mut ip_range = IpRange::new();
+
+    while let Some(res) = lines.next() {
+        let line = res?;
+        ip_range.add(Ipv4Net::from_str(&line)?);
+    }
+
+    ip_range.simplify();
+    Ok(ip_range)
+}
+
+#[no_mangle]
+extern "C" fn create_hooks(fubuki_ctx: ExternalContext) -> *mut HookHandle {
+    let lan_ip = get_interface_addr(SocketAddr::new([1, 1, 1, 1].into(), 53)).unwrap();
+    let device = find_interface(lan_ip).unwrap();
+    let device = Arc::new(device);
+    let rt = Runtime::new().unwrap();
+
+    let netstack_sink_tx = rt.block_on(async {
+        let (stack, tcp_listener, udp_socket) = netstack_lwip::NetStack::new().unwrap();
+        let (stack_sink, stack_stream) = stack.split();
+        let (netstack_sink_tx, netstack_sink_rx) = tokio::sync::mpsc::channel(1024);
+
+        tokio::spawn({
+            let device = device.clone();
+            async move {
+                if let Err(e) = tcp_inbound_handler(tcp_listener, device).await {
+                    error!("tcp_inbound_handler error: {:?}", e);
+                }
+            }
+        });
+
+        tokio::spawn(async move {
+            if let Err(e) = udp_inbound_handler(udp_socket, device).await {
+                error!("udp_inbound_handler error: {:?}", e);
+            }
+        });
+
+        tokio::spawn(netstatck_handler(stack_stream, fubuki_ctx));
+
+        tokio::spawn(async move {
+            if let Err(e) = netstatck_sink_handler(stack_sink, netstack_sink_rx).await {
+                error!("netstatck_sink_handler error: {:?}", e);
+            }
+        });
+
+        netstack_sink_tx
+    });
+
+    let file_path = std::env::var("FUBUKI_RULES_FILE").unwrap();
+    let rule = Rule::Match(parse_rules(&file_path).unwrap());
+
+    let hook = HookHandle {
+        _rt: rt,
+        netstatck_sink_handler_tx: netstack_sink_tx,
+        rule,
+    };
+
+    Box::into_raw(Box::new(hook))
+}
+
+#[no_mangle]
+extern "C" fn drop_hooks(handle: *mut HookHandle) {
+    let _ = unsafe { Box::from_raw(handle) };
+}
+
+#[repr(C)]
+struct Input {
+    direction: Direction,
+    packet: *mut u8,
+    len: usize,
+}
+
+#[repr(C)]
+#[allow(unused)]
+enum PacketRecvOutput {
+    Accept = 0,
+    Drop,
+}
+
+#[no_mangle]
+extern "C" fn packet_recv(handle: &HookHandle, input: &mut Input, output: &mut PacketRecvOutput) {
+    if input.direction != Direction::Output {
+        return;
+    }
+
+    let packet = unsafe { std::slice::from_raw_parts(input.packet, input.len) };
+    let dst = match get_ip_dst_addr(packet) {
+        Ok(v) => v,
+        Err(e) => {
+            error!("parse ipv4 packet error: {}", e);
+            return;
+        }
+    };
+
+    if !handle.rule.is_proxy(&dst) {
+        return;
+    }
+
+    let _ = handle.netstatck_sink_handler_tx.try_send(packet.to_vec());
+    *output = PacketRecvOutput::Drop;
+}
---
+++ src/net.rs
@@ -0,0 +1,196 @@
+use socket2::TcpKeepalive;
+use std::io::Result;
+use std::io;
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr};
+use std::ops::Range;
+use std::time::Duration;
+
+pub trait SocketExt {
+    fn set_keepalive(&self) -> Result<()>;
+
+    fn set_recv_buffer_size(&self, size: usize) -> Result<()>;
+
+    fn set_send_buffer_size(&self, size: usize) -> Result<()>;
+
+    fn bind_device(&self, _interface: &str, _ipv6: bool) -> Result<()>;
+}
+
+const TCP_KEEPALIVE: TcpKeepalive = TcpKeepalive::new().with_time(Duration::from_secs(120));
+
+#[cfg(any(target_os = "linux", target_os = "android"))]
+fn bind_device<T: std::os::unix::io::AsFd>(
+    socket: &T,
+    interface: &str,
+    _ipv6: bool
+) -> Result<()> {
+    let socket = socket2::SockRef::from(socket);
+    socket.bind_device(Some(interface.as_bytes()))
+}
+
+#[cfg(target_os = "macos")]
+fn bind_device<T: std::os::unix::io::AsFd>(
+    socket: &T,
+    interface: &str,
+    ipv6: bool
+) -> Result<()> {
+    use std::num::NonZeroU32;
+
+    let socket = socket2::SockRef::from(socket);
+
+    let index = netconfig::Interface::try_from_name(interface)
+        .and_then(|i| i.index())
+        .map_err(|e| std::io::Error::new(io::ErrorKind::Other, e.to_string()))?;
+
+    if ipv6 {
+        socket.bind_device_by_index_v6(NonZeroU32::new(index))
+    } else {
+        socket.bind_device_by_index_v4(NonZeroU32::new(index))
+    }
+}
+
+#[cfg(windows)]
+fn bind_device<T: std::os::windows::io::AsSocket>(
+    socket: &T,
+    interface: &str,
+    ipv6: bool,
+) -> Result<()> {
+    use std::os::windows::io::AsRawSocket;
+    use netconfig::sys::InterfaceExt;
+
+    let index = netconfig::Interface::try_from_alias(interface)
+        .and_then(|i| i.index())
+        .map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?;
+
+    let raw = socket.as_socket().as_raw_socket();
+
+    unsafe {
+        let code = if ipv6 {
+            windows::Win32::Networking::WinSock::setsockopt(
+                windows::Win32::Networking::WinSock::SOCKET(raw as usize),
+                windows::Win32::Networking::WinSock::IPPROTO_IPV6.0,
+                windows::Win32::Networking::WinSock::IPV6_UNICAST_IF,
+                Some(&index.to_be_bytes()),
+            )
+        } else {
+            windows::Win32::Networking::WinSock::setsockopt(
+                windows::Win32::Networking::WinSock::SOCKET(raw as usize),
+                windows::Win32::Networking::WinSock::IPPROTO_IP.0,
+                windows::Win32::Networking::WinSock::IP_UNICAST_IF,
+                Some(&index.to_be_bytes()),
+            )
+        };
+
+        if code != 0 {
+            return Err(io::Error::last_os_error());
+        }
+    };
+
+    Ok(())
+}
+
+pub fn get_interface_addr(dest_addr: SocketAddr) -> Result<IpAddr> {
+    let bind_addr = match dest_addr {
+        SocketAddr::V4(_) => IpAddr::V4(Ipv4Addr::UNSPECIFIED),
+        SocketAddr::V6(_) => IpAddr::V6(Ipv6Addr::UNSPECIFIED),
+    };
+
+    let socket = std::net::UdpSocket::bind((bind_addr, 0))?;
+    socket.connect(dest_addr)?;
+    let addr = socket.local_addr()?;
+    Ok(addr.ip())
+}
+
+#[cfg(any(target_os = "windows", target_os = "linux", target_os = "macos"))]
+pub fn find_interface(ip: IpAddr) -> Result<String> {
+    #[allow(unused_imports)]
+    use netconfig::sys::InterfaceExt;
+
+    let ifs = netconfig::list_interfaces()
+        .map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?;
+
+    for inter in ifs {
+        let addrs = inter.addresses()
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?;
+
+        for addr in addrs {
+            if addr.addr() == ip {
+                #[cfg(windows)]
+                    let if_name = inter.alias();
+
+                #[cfg(unix)]
+                    let if_name = inter.name();
+
+                return if_name.map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))
+            }
+        }
+    }
+
+    Err(io::Error::new(io::ErrorKind::InvalidInput, "interface not found"))
+}
+
+macro_rules! build_socket_ext {
+    ($type:path) => {
+        impl<T: $type> SocketExt for T {
+            fn set_keepalive(&self) -> Result<()> {
+                let sock_ref = socket2::SockRef::from(self);
+                sock_ref.set_tcp_keepalive(&TCP_KEEPALIVE)
+            }
+
+            fn set_recv_buffer_size(&self, size: usize) -> Result<()> {
+                let sock_ref = socket2::SockRef::from(self);
+                sock_ref.set_recv_buffer_size(size)
+            }
+
+            fn set_send_buffer_size(&self, size: usize) -> Result<()> {
+                let sock_ref = socket2::SockRef::from(self);
+                sock_ref.set_send_buffer_size(size)
+            }
+
+            fn bind_device(&self, interface: &str, ipv6: bool) -> Result<()> {
+                bind_device(self, interface, ipv6)
+            }
+        }
+    };
+}
+
+#[cfg(windows)]
+build_socket_ext!(std::os::windows::io::AsSocket);
+
+#[cfg(unix)]
+build_socket_ext!(std::os::unix::io::AsFd);
+
+macro_rules! get {
+    ($slice: expr, $index: expr, $error_msg: expr) => {
+        $slice
+            .get($index)
+            .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, $error_msg))?
+    };
+    ($slice: expr, $index: expr) => {
+        get!($slice, $index, "decode error")
+    };
+}
+
+#[allow(unused)]
+const SRC_ADDR: Range<usize> = 12..16;
+const DST_ADDR: Range<usize> = 16..20;
+
+pub fn get_ip_dst_addr(ip_packet: &[u8]) -> Result<Ipv4Addr> {
+    let mut buff = [0u8; 4];
+    buff.copy_from_slice(get!(
+        ip_packet,
+        DST_ADDR,
+        "get packet source address failed"
+    ));
+    Ok(Ipv4Addr::from(buff))
+}
+
+#[allow(unused)]
+pub fn get_ip_src_addr(ip_packet: &[u8]) -> Result<Ipv4Addr> {
+    let mut buff = [0u8; 4];
+    buff.copy_from_slice(get!(
+        ip_packet,
+        SRC_ADDR,
+        "get packet destination address failed"
+    ));
+    Ok(Ipv4Addr::from(buff))
+}
